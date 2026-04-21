1552f7f460364c9cc06bcf950be63b1c9d874d5e
AvniIsrani1 <151782981+AvniIsrani1@users.noreply.github.com>
2026-04-21 16:24:46 -0700

    project 3 tasks

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Task 1: Verification Log
+console.log("Status Manager Started");
 
 // Global variable setup (required for Task 10 using setInterval/clearInterval)
 let intervalId = null;
@@ -16,11 +17,13 @@ const itemList = document.getElementById("item-list");
 // --- Task 3: Selecting and Changing Inner HTML ---
 // Write the code here to select the mainTitle and update its innerHTML:
 // Example: mainTitle.innerHTML = "New Title";
+mainTitle.innerHTML = "DOM Project: Ready!"
 
 /* ======================================= */
 // --- Task 4: Attribute Modification ---
 // Write the code here to use setAttribute() on the toggleButton element
 // to add the required 'data-action' attribute.
+toggleButton.setAttribute("data-action", "status-toggle")
 
 /* ======================================= */
 // --- Task 9: Looping and Applying Changes ---
@@ -33,8 +36,54 @@ const itemList = document.getElementById("item-list");
 // Define the functions (e.g., toggleStatus, createTimestamp) and event listeners
 // here to handle the click event on the toggleButton [6, 7].
 
+// Task 5: Toggle Function
+function toggleStatus(e){
+    e.preventDefault(); // Task 6: Prevent Default Behavior
+    statusOutput.classList.toggle("hidden"); // add/remove hidden class
+    if(!statusOutput.classList.contains("hidden")){
+        // Task 7: Inline Styles
+        // Set main-title element's background color to yello
+        mainTitle.style.backgroundColor = "yellow"; 
+        createTimestamp();
+    } else {
+        mainTitle.style.backgroundColor = "";
+    }
+}
+toggleButton.addEventListener('click', toggleStatus);
+
+// Task 8: Dynamic Element Creation
+function createTimestamp(){
+    const spanElement = document.createElement("span"); // create new span
+    spanElement.innerHTML = new Date().toLocaleTimeString(); // set inner HTML to current time
+    statusOutput.appendChild(spanElement); // add span inside status-output div
+}
+
+// Task 9: Loops and Node Lists
+function highlightListItems(){
+    const itemList = document.querySelectorAll('li'); // select all list items in item-list
+    itemList.forEach((item)=> {
+        item.style.color = "blue" // set inline color style to blue
+    });
+}
+highlightListItems();
+
+
+
 /* ======================================= */
 // --- Task 10: Timed Animation ---
 // Define the startFlashing() and stopFlashing() functions using
 // setInterval() and clearInterval() [8, 9], and bind them to the
 // timerButton using addEventListener for 'click' and 'dblclick' [10].
+
+function startFlashing(){
+    intervalId = setInterval(()=>{
+        controlPanel.classList.toggle("hidden");
+    }, 500)
+}
+
+function stopFlashing(){
+    clearInterval(intervalId);
+}
+
+timerButton.addEventListener("click", startFlashing);
+timerButton.addEventListener("dblclick", stopFlashing);
